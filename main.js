c2da5b4c2cf0f1969395c000f1dba2e60f3d71f9
Kamogelo <ekmogasoa@gmail.com>
2025-06-01 18:18:01 +0200

    initial commiy

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+const num = 5;
+const str = "Hello, TypeScript!";
+console.log(`Number: ${num} and String: ${str}`);
+let user = ["John Doe", "Jane Smith", "John Doe"];
+if (user.includes("John Doe")) {
+    console.log("User John Doe exists in the array.");
+}
---
+++ main.ts
@@ -0,0 +1,10 @@
+const num: number = 5;
+const str: string = "Hello, TypeScript!";
+
+console.log(`Number: ${num} and String: ${str}`);
+
+let user: string[] = ["John Doe", "Jane Smith", "John Doe"];
+
+if (user.includes("John Doe")) {
+  console.log("User John Doe exists in the array.");
+}
