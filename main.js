e8b3987f661c8951de57c1bcf2e6cfdf7301b6d3
Kamogelo <ekmogasoa@gmail.com>
2025-06-01 18:24:31 +0200

    Stop tracking main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const num = 5;
-const str = "Hello, TypeScript!";
-console.log(`Number: ${num} and String: ${str}`);
-let user = ["John Doe", "Jane Smith", "John Doe"];
-if (user.includes("John Doe")) {
-    console.log("User John Doe exists in the array.");
-}
