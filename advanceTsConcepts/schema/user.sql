6f6272f2bcad5513d56aafcf033014c3db631e40
Kamogelo <ekmogasoa@gmail.com>
2025-06-18 19:55:24 +0200

    added new table

--- advanceTsConcepts/schema/user.sql
+++ advanceTsConcepts/schema/user.sql
@@ -5,25 +5,34 @@ CREATE TABLE users(
     password VARCHAR(100) NOT NULL
 );
 
-INSERT INTO users(username, email, password);
-values('james oddy', 'james17@gmail.com', 'michBeni30')
+INSERT INTO users(username, email, password)
+values('james oddy', 'james17@gmail.com', 'michBeni30');
 
 UPDATE users
 SET username = 'Nenemis centauri'
-WHERE id = 1
+WHERE id = 1;
 
 CREATE TABLE items(
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY NOT NULL,
     stock int NOT NULL,
-    arrival DATE NOT NULL,
+    arrival TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     country VARCHAR(50) NOT NULL,
     user_id int NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-INSERT INTO items(stock, arrival, country, user_id);
-VALUES(10, '2025-05-03', 'South Africa', 1)
+INSERT INTO items(stock, arrival, country, user_id)
+VALUES(10, '2025-05-03', 'South Africa', 1);
 
+CREATE TABLE item_shelf(
+    id SERIAL PRIMARY KEY,
+    shelf INT NOT NULL,
+    item_id INT NOT NULL,
+    FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
+);
+
+INSERT INTO item_shelf(shelf, item_id)
+VALUES(1,1)
 
 app.get("/products",(req,res)=>{
     const productData = req.body["item"]
