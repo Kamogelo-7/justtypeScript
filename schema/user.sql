d0b808d44d57b6996cfa59db17676c1278f6b777
Kamogelo <ekmogasoa@gmail.com>
2025-06-19 16:55:55 +0200

    added new data

--- schema/user.sql
+++ schema/user.sql
@@ -1,12 +1,18 @@
 CREATE TABLE users(
     id SERIAL PRIMARY KEY NOT NULL,
     username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL
 );
 
-INSERT INTO users(username, email, password)
-values('james oddy', 'james17@gmail.com', 'michBeni30');
+INSERT INTO users (username, email, password)
+VALUES
+  ('James oddy', 'james17@gmail.com', 'michBeni30'),
+  ('Eren derack', 'ederack@gmail.com', 'lu#2artdu'),
+  ('Scimmia laplace', 'laprack@gmail.com', 'lupdeni34'),
+  ('Manny audituri', 'kimishaudi@gmail.com', 'systAnayst2*'),
+  ('Meteo Leoni', 'meteoLeoNene@gmail.com', 'saviordarleni^8'),
+  ('Lucia Caminos', 'camiCon23@gmail.com', 'Blockblazer#%');
 
 UPDATE users
 SET username = 'Nenemis centauri'
@@ -21,8 +27,18 @@ CREATE TABLE items(
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-INSERT INTO items(stock, country, user_id)
-VALUES(10, 'South Africa', 1);
+INSERT INTO items (stock, country, user_id)
+VALUES
+  (10, 'South Africa', 1),
+  (7, 'Saudi Arabia', 2),
+  (8, 'Japan', 3),
+  (5, 'Jamaica', 4),
+  (17, 'Colombia', 5),
+  (5, 'Saint Lucia',6);
+
+SELECT id, stock, country FROM items
+WHERE country LIKE 'S%'
+ORDER BY id ASC
 
 CREATE TABLE shelves(
     id SERIAL PRIMARY KEY,
@@ -33,10 +49,3 @@ CREATE TABLE shelves(
 
 INSERT INTO shelves(shelf_position, item_id)
 VALUES(1,1)
-
-app.get("/products",(req,res)=>{
-    const productData = req.body["item"]
-    productData.find((product)=> {
-        if(product.id !== productdata.id) return null
-    })
-})
